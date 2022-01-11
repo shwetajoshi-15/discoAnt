85420579ec84cfef47f5fcfe7b33dcc48b9668a7
shwetajoshi-15 <54519592+shwetajoshi-15@users.noreply.github.com>
2022-01-11 14:22:53 +1100

    Update discoAnt.sh

--- discoAnt.sh
+++ discoAnt.sh
@@ -13,7 +13,7 @@
 ## 7. Align the samples fasta files to the metagene
 ## 8. Generate counts/TPM based on the alignments to the metagene
 
-source discoAnt_params_"$GENE".txt
+source discoAnt_params_CALCR.txt
 export PYTHONPATH="$PROGRAMS/cDNA_Cupcake/sequence/:$PYTHONPATH"
 
 echo "Making folders"
@@ -23,43 +23,19 @@ mkdir -p $DISCOANT/$GENE/fasta_stats
 mkdir -p $DISCOANT/$GENE/minimap2
 mkdir -p $DISCOANT/$GENE/minimap2_target
 mkdir -p $DISCOANT/$GENE/flair
+mkdir -p $DISCOANT/$GENE/flair/$GENE
 mkdir -p $DISCOANT/$GENE/sqanti3
 mkdir -p $DISCOANT/$GENE/flair_metagene_minimap2
 mkdir -p $DISCOANT/$GENE/flair_metagene_salmon
-
+mkdir -p $DISCOANT/$GENE/flair/collapse
+mkdir -p $DISCOANT/$GENE/flair/correct
+mkdir -p $DISCOANT/$GENE/flair_metagene_counts
+mkdir -p $DISCOANT/$GENE/plots
 
 ##########                                          ##########
 ##########  1. Quality control for the fasta files  ##########
 ##########                                          ##########
 
-echo "Extracting the number of reads and read lengths"
-
-	basename -s .fa $FASTA/*.fa | sed 's/^.*bar/bar/g' > $DISCOANT/$GENE/fasta_stats/barcodes.txt
-
-	file=$DISCOANT/$GENE/fasta_stats/$GENE_stats.COMPLETED
-
-	if [[ ! -f $DISCOANT/$GENE/fasta_stats/"$GENE"_stats.COMPLETED ]]
-	then
-
-	for filename in $FASTA/*.fa
-	do
-	base=$(basename $filename .fa)
-	echo "On sample : $base"
-	cat $FASTA/${base}.fa | grep -c "^>" >> $DISCOANT/$GENE/fasta_stats/"$GENE"_tmp1.txt
-	paste $DISCOANT/$GENE/fasta_stats/barcodes.txt $DISCOANT/$GENE/fasta_stats/"$GENE"_tmp1.txt | column -s $'\t' -t > $DISCOANT/$GENE/fasta_stats/"$GENE"_total_number_of_reads.txt
-
-	mkdir -p $DISCOANT/$GENE/fasta_stats/"$GENE"_read_lengths
-	bioawk -c fastx '{print length($seq)}' $FASTA/${base}.fa > $DISCOANT/$GENE/fasta_stats/"$GENE"_read_lengths/${base}.txt
-
-	awk '{/>/&&++a||b+=length()}END{print b/a}' $FASTA/${base}.fa >> $DISCOANT/$GENE/fasta_stats/"$GENE"_tmp2.txt
-	paste $DISCOANT/$GENE/fasta_stats/barcodes.txt $DISCOANT/$GENE/fasta_stats/"$GENE"_tmp2.txt | column -s $'\t' -t > $DISCOANT/$GENE/fasta_stats/"$GENE"_mean_read_lengths.txt
-
-	rm $DISCOANT/$GENE/fasta_stats/*tmp* && rm $DISCOANT/$GENE/fasta_stats/barcodes.txt && touch $DISCOANT/$GENE/fasta_stats/"$GENE"_stats.COMPLETED
-	done
-
-	else
-	echo "FASTA stats are present in $DISCOANT/$GENE/fasta_stats"
-	fi
 
 
 ##########                                                      ##########
@@ -81,8 +57,9 @@ echo "minimap2 - Mapping fasta files to genome"
 	samtools view -S -h -b $DISCOANT/$GENE/minimap2/${base}.sam | samtools sort - > $DISCOANT/$GENE/minimap2/${base}_sorted.bam
 	samtools view -h -F 2308 $DISCOANT/$GENE/minimap2/${base}_sorted.bam | samtools sort - > $DISCOANT/$GENE/minimap2/${base}_pri_sorted.bam
 	done
-	touch $DISCOANT/$GENE/minimap2/"$GENE"_sorted_pri_align.COMPLETED
 	
+	touch $DISCOANT/$GENE/minimap2/"$GENE"_sorted_pri_align.COMPLETED
+
 	else
 	echo "Alignments to the genome are present in $DISCOANT/$GENE/minimap2"
 	fi
@@ -100,9 +77,11 @@ echo "Extracting alignments to the target region/gene"
 	do
 	base=$(basename $filename .fa)
 	echo "On sample : $base"
+	
 	samtools index $DISCOANT/$GENE/minimap2/${base}_pri_sorted.bam
 	samtools view -h $DISCOANT/$GENE/minimap2/${base}_pri_sorted.bam "$CHR:$GENE_START-$GENE_END" > $DISCOANT/$GENE/minimap2_target/${base}_pri_tar_sorted.bam
 	done
+	
 	touch $DISCOANT/$GENE/minimap2_target/"$GENE"_sorted_pri_tar_align.COMPLETED
 
 	else
@@ -136,9 +115,12 @@ echo "Merging minimap2 primary alignments"
 echo "FLAIR - correct and collapse"
 
 	cat $FASTA/*.fa > $ALL/all.fa
+
 	python $PROGRAMS/flair/bin/bam2Bed12.py -i $DISCOANT/$GENE/minimap2_target/"$GENE"_merged_sorted.bam > $DISCOANT/$GENE/minimap2_target/"$GENE"_merged_sorted.bed
-	python $PROGRAMS/flair/flair.py correct -q $DISCOANT/$GENE/minimap2_target/"$GENE"_merged_sorted.bed -c $REF_HG38/chrom_sizes.txt -f $REF_HG38/gencode.v35.annotation.gtf -g $REF_HG38/GRCh38.p13.genome_edit.fa -o $DISCOANT/$GENE/flair/$GENE
-	python $PROGRAMS/flair/flair.py collapse -r $ALL/all.fa -q $DISCOANT/$GENE/flair/"$GENE"_all_corrected.psl -f $REF_HG38/gencode.v35.annotation.gtf -g $REF_HG38/GRCh38.p13.genome_edit.fa -o $DISCOANT/$GENE/flair/"$GENE"_collapse_500 --temp_dir $DISCOANT/$GENE/flair -s 500
+	python $PROGRAMS/flair/flair.py correct -q $DISCOANT/$GENE/minimap2_target/"$GENE"_merged_sorted.bed -c $REF_HG38/chrom_sizes.txt -f $REF_HG38/gencode.v35.annotation.gtf -g $REF_HG38/GRCh38.p13.genome_edit.fa -o $DISCOANT/$GENE/flair/correct/default
+
+	python $PROGRAMS/flair/flair.py collapse -r $ALL/all.fa -q $DISCOANT/$GENE/flair/correct/default_all_corrected.bed -f $REF_HG38/gencode.v35.annotation.gtf -g $REF_HG38/GRCh38.p13.genome_edit.fa -o $DISCOANT/$GENE/flair/collapse/default_500 --temp_dir $DISCOANT/$GENE/flair -s 500
+	python $PROGRAMS/flair/flair.py collapse -r $ALL/all.fa -q $DISCOANT/$GENE/flair/correct/default_all_corrected.bed -f $REF_HG38/gencode.v35.annotation.gtf -g $REF_HG38/GRCh38.p13.genome_edit.fa -o $DISCOANT/$GENE/flair/collapse/default_100 --temp_dir $DISCOANT/$GENE/flair -s 100
 
 ##########                          ##########
 ########## 6. Transcript annotation ##########
@@ -149,13 +131,21 @@ echo "Annotated transcripts are present in $DISCOANT/$GENE/sqanti3"
 	if [[ ! -f $DISCOANT/$GENE/sqanti3/"$GENE"_sqanti3.COMPLETED ]]
 	then
 	python $PROGRAMS/SQANTI3-1.3/sqanti3_qc.py \
-	--gtf $DISCOANT/$GENE/flair/"$GENE"_collapse_500.isoforms.gtf \
+	--gtf $DISCOANT/$GENE/flair/collapse/default_500.isoforms.gtf \
 	$REF_HG38/gencode.v35.annotation.gtf $REF_HG38/GRCh38.p13.genome_edit.fa \
 	--cage_peak $REF_HG38/refTSS_v3.3_human_coordinate.hg38.bed \
 	--polyA_peak $REF_HG38/atlas.clusters.2.0.GRCh38.96.bed --polyA_motif_list $REF_HG38/human.polyA.list.txt \
-	-d $DISCOANT/$GENE/sqanti3 -o "$GENE"
+	-d $DISCOANT/$GENE/sqanti3 -o "$GENE"_500
+	
+	python $PROGRAMS/SQANTI3-1.3/sqanti3_qc.py \
+	--gtf $DISCOANT/$GENE/flair/collapse/default_100.isoforms.gtf \
+	$REF_HG38/gencode.v35.annotation.gtf $REF_HG38/GRCh38.p13.genome_edit.fa \
+	--cage_peak $REF_HG38/refTSS_v3.3_human_coordinate.hg38.bed \
+	--polyA_peak $REF_HG38/atlas.clusters.2.0.GRCh38.96.bed --polyA_motif_list $REF_HG38/human.polyA.list.txt \
+	-d $DISCOANT/$GENE/sqanti3 -o "$GENE"_100
 
 	touch $DISCOANT/$GENE/sqanti3/"$GENE"_sqanti3.COMPLETED
+	
 	else
 	echo "Annotated transcripts are present in $DISCOANT/$GENE/sqanti3"
 	fi
@@ -174,11 +164,11 @@ echo "Mapping the samples to the metagene (transcriptome) generated by flair"
 	base=$(basename $filename _pri_sorted.bam)
 	echo "On sample : $base"
 
-	minimap2 -ax map-ont $DISCOANT/$GENE/flair/"$GENE"_collapse_500.isoforms.fa $FASTA/${base}.fa > $DISCOANT/$GENE/flair_metagene_minimap2/${base}.sam
+	minimap2 -ax map-ont $DISCOANT/$GENE/flair/collapse/default_500.isoforms.fa $FASTA/${base}.fa > $DISCOANT/$GENE/flair_metagene_minimap2/${base}.sam
 	samtools view -S -h -b $DISCOANT/$GENE/flair_metagene_minimap2/${base}.sam | samtools sort - > $DISCOANT/$GENE/flair_metagene_minimap2/${base}_sorted.bam
 	samtools view -h -F 2308 $DISCOANT/$GENE/flair_metagene_minimap2/${base}_sorted.bam | samtools sort - > $DISCOANT/$GENE/flair_metagene_minimap2/${base}_pri_sorted.bam
 	samtools index $DISCOANT/$GENE/flair_metagene_minimap2/${base}_pri_sorted.bam
-	
+
 	done
 	touch $DISCOANT/$GENE/flair_metagene_minimap2/"$GENE"_pri_sorted_align.COMPLETED
 
@@ -186,25 +176,17 @@ echo "Mapping the samples to the metagene (transcriptome) generated by flair"
 	echo "Alignments to the metagene are present in $DISCOANT/$GENE/flair_metagene_minimap2"
 	fi
 
-##########                                         ##########
-########## 8. Quantification based on the metagene ##########
-##########                                         ##########
+
+##########                                                    ##########
+########## 8. Quantification based on the metagene with Flair ##########
+##########                                                    ##########
 
 echo "Quantifying transcripts based on primary alignments to the metagene"
 
-	if [[ ! -f $DISCOANT/$GENE/flair_metagene_minimap2/"$GENE"_quant.COMPLETED ]]
-	then
+	python $PROGRAMS/flair/flair.py quantify -r $DISCOANT/$GENE/reads_manifest_"$GENE".tsv -i $DISCOANT/$GENE/flair/collapse/default_500.isoforms.fa -o $DISCOANT/$GENE/flair_metagene_counts/default --tpm
 
-	for filename in $DISCOANT/$GENE/minimap2/*_pri_sorted.bam
-	do
-	base=$(basename $filename _pri_sorted.bam)
-	echo "On sample : $base"
+##########                                       ##########
+########## 9. Plotting isoform usage with flair ##########
+##########                                       ##########
 
-	salmon quant -t $DISCOANT/$GENE/flair/"$GENE"_collapse_500.isoforms.fa -l A -a $DISCOANT/$GENE/flair_metagene_minimap2/${base}_pri_sorted.bam -o $DISCOANT/$GENE/flair_metagene_salmon/${base}
-
-	done
-	touch $DISCOANT/$GENE/flair_metagene_minimap2/"$GENE"_quant.COMPLETED
-
-	else
-	echo "Transcript quantification is present in $DISCOANT/$GENE/sqanti3"
-	fi
+python $PROGRAMS/flair/bin/plot_isoform_usage.py $DISCOANT/$GENE/flair/collapse/default_500.isoforms.bed $DISCOANT/$GENE/flair_metagene_counts/default $GENE_ID $DISCOANT/$GENE/plots/"$GENE"_isoform_plots
