38c73f863ece6efe5a83a362a45adfeafc0cf0d2
shwetajoshi-15 <54519592+shwetajoshi-15@users.noreply.github.com>
2022-07-21 00:41:09 +1000

    Update discoAnt_setup.sh

--- discoAnt_setup.sh
+++ discoAnt_setup.sh
@@ -3,6 +3,8 @@
 source discoAnt_params.txt
 conda env create -f discoAnt.yml
 
+conda activate discoAnt
+
 ## Installing programs
 
 mkdir -p $PROGRAMS
@@ -45,5 +47,6 @@ mkdir -p $REF_HG38
         
         sed -i 's/^/chr/' $REF_HG38/atlas.clusters.2.0.GRCh38.96.bed
         
+conda deactivate
         
 
